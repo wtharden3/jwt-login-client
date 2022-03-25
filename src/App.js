db2617bd4ec2b99a9a76bb0d36188e6340168a8d
Whitney Harden <wtharden320@gmail.com>
2022-03-25 06:47:32 -0400

    starting react-router-dom routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      jwt-login-client
+      <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
